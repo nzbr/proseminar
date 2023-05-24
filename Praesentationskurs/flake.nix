c65d374aa718ec1f7e02961de0c8999eb7876b3c
nzbr <mail@nzbr.de>
2023-05-24 22:07:12 +0200

    Präsentationskurs final

--- Praesentationskurs/flake.nix
+++ Praesentationskurs/flake.nix
@@ -12,7 +12,7 @@
       in
       {
         packages = {
-          praesentationskurs = pkgs.callPackage (./. + "/Praesentationskurs") copperflame.packages.${system};
+          praesentationskurs = pkgs.callPackage ./. copperflame.packages.${system};
         };
       }
     )) // {
