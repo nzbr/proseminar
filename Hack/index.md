fc93a54f063b47ecc13b1b9590aaa02b792919dc
nzbr <mail@nzbr.de>
2023-07-11 03:58:27 +0200

    Hack: content

--- Hack/index.md
+++ Hack/index.md
@@ -3,17 +3,209 @@ title: "Pandocode"
 author: "Nico Jansen"
 date: 12.07.2023
 lang: "de-DE"
+showSlideNumber: print
 controlsTutorial: false
 css: "theme.scss"
 csl: "../thirdparty/csl-styles/ieee.csl"
-# bibliography:
-#    - "hauptvortrag.bib"
+bibliography:
+   - "hack.bib"
+no-title-slide: true
+header-includes:
+    - <script src="./js/index.tsx" type="module"></script>
+include-before:
+    - |
+        <section id="title-slide">
+            <h2 class="title">Pandocode</h2>
+            <p class="author">Nico Jansen</p>
+            <p class="date">11.07.2023</p>
+            <div id="connection-test"></div>
+        </section>
 ---
 
-# Folie 1
+# Was
 
+- Python Code $\to$ LaTeX Pseudocode
+- Implementiert in Python
+- Standalone oder Pandoc Filter
+  - panflute[@pypi-panflute] als Pandoc Filter Library
 
-# Quellen
+# Warum
 
-::: {#refs}
+**Warum wurde Pandocode geschrieben?**
+
+- DAP2 Übungsblätter
+- Algorithmen in Python getestet
+- LaTeX Pseudocode[@ctan-algorithmicx] Syntax ist umständlich
+
+<br/>
+
+**Warum wird es vorgestellt?**
+
+- Gute Technologien für schnelle Hacks
+  - Python
+  - Pandoc und Filter
+  - CGI
+
+# Verwendung (Standalone)
+
+:::side-by-side
+```{.editor #standalone-editor}
+def f(x):
+    result = 1
+    for x in range(10):
+        print(result)
+        result *= x
+    return result
+
+print(f(10))
+```
+<div class="output" for="standalone-editor" type="code"></div>
+:::
+
+:::sidebar
+
+**Control Sequences**:
+
+- `#$begin` / `#$end`
+    - Nur Code dazwischen verarbeiten
+- `#$+ <text>`
+    - Zeile einfügen
+- `#$-`
+    - Nächste Zeile entfernen
+- `#$= <text>`
+    - Nächste Zeile ersetzen
+
+<br/>
+
+**Aufruf**:
+
+```sh
+$ pandocode code.py
+```
+:::
+
+# Pandoc
+
+- Dokumentenkonverter
+- 42 Eingabe- und 61 Ausgabeformate[@pandoc]
+
+![](./diagram-pandoc.png?as=avif)
+
+```sh
+$ pandoc <eingabe> -o <ausgabe>
+$ pandoc <eingabe> --template <template> -o <ausgabe>
+```
+
+# Pandoc Filter
+
+- Ausführbare Programme
+- Verändern das Dokument
+  - Manipulation des AST
+
+![](./diagram-filters.png?as=avif)
+
+```sh
+pandoc <eingabe> --filter <filter> -o <ausgabe>
+pandoc <eingabe> --filter <filter> --template <template> -o <ausgabe>
+```
+
+```sh
+pandoc <eingabe> --to=json | <filter> | pandoc --from=json -o <ausgabe>
+pandoc <eingabe> --to=json | <filter> | pandoc --from=json --template <template> -o <ausgabe>
+```
+
+# Verwendung (Filter)
+
+:::side-by-side
+<pre class="editor" id="filter-editor"><code>
+---
+header-includes:
+- \usepackage{algpseudocode}
+---
+
+# Markdown!
+
+```pseudo 
+def f(x):
+    result = 1
+    for x = 1 to x:
+        result = result * x
+    return result
+```
+
+Die Funktion $f(x)$ multipliziert
+die Zahlen von $1$ bix $x$ mit einander
+und gibt das Ergebnis zurück
+</code></pre>
+<div class="output" for="filter-editor" type="mdrender"></div>
+:::
+
+:::sidebar
+
+- Wandelt alle Code-Blöcke des Typs `pseudo` um
+
+**Pseudocode Block**:
+
+    ```pseudo 
+    def id(x):
+        return x
+    ```
+
+<br/>
+
+**Aufruf**:
+
+```sh
+$ pandoc dokument.md \
+    --filter pandocode \
+    -o dokument.pdf
+```
+:::
+
+# Pandocode Live
+
+- Wrapper um Pandocode
+- HTTP-API
+  - Python Code $\to$ POST-Request $\to$ LaTeX
+- CGI (Common Gateway Interface)
+  - Größtenteils verdrängt
+  - **Aber**: Sehr einfach, perfekt für schnelle Hacks
+    - Webserver führt Executable/Scripts aus
+    - Anfragedaten per STDIN und Umgebungsvariablen
+    - Antwort per STDOUT
+
+# Links
+
+<div style="display: flex; justify-content: space-between">
+<div class="link-box" style="margin: 0; width: calc(50% - 2.25em)">
+<div class="text">
+<strong>Pandocode</strong>
+
+<a href="https://github/nzbr/pandocode">https://github.com/nzbr/pandocode</a>
+</div>
+</div>
+
+<div class="link-box" style="margin: 0; width: calc(50% - 2.25em)">
+<div class="text">
+<strong>Pandocode Live</strong>
+
+<a href="https://pandocode.nzbr.de">https://pandocode.nzbr.de</a>
+</div>
+</div>
+</div>
+
+:::link-box
+<div class="text">
+<strong>Alle Vorträge</strong>
+
+<a href="https://github.com/nzbr/proseminar">https://github.com/nzbr/proseminar</a>
+</div>
+:::
+
+:::link-box
+<div class="text">
+<strong>Andere Quellen</strong>
+
+<div id="refs"></div>
+</div>
 :::
---
+++ Hack/js/index.tsx
@@ -0,0 +1,166 @@
+//@ts-ignore
+import {c, g, r, x} from '@xeserv/xeact';
+import {editor} from 'monaco-editor';
+import IStandaloneEditorConstructionOptions = editor.IStandaloneEditorConstructionOptions;
+import IStandaloneCodeEditor = editor.IStandaloneCodeEditor;
+
+const backendUrl = 'https://pandocode.nzbr.de';
+
+export default r(() => {
+    const progress = <progress value={0} max={1}/>
+    const resultText = <div>...</div>
+    g('connection-test').append(<div>
+        <div>Verbindung</div>
+        {progress}
+        {resultText}
+    </div>);
+
+    editor.defineTheme('copperflame', {
+        base: 'vs',
+        inherit: true,
+        rules: [],
+        colors: {
+            "editor.background": "#00000000",
+        }
+    });
+
+    const monacoCommonOptions: IStandaloneEditorConstructionOptions = {
+        automaticLayout: true,
+        theme: "copperflame",
+        fontFamily: 'JetBrains Mono',
+        fontSize: 16,
+        lineNumbers: "off",
+        minimap: {
+            enabled: false
+        },
+        scrollBeyondLastLine: false,
+    };
+
+    const editors: Record<string, IStandaloneCodeEditor> = {};
+    [...c('editor')].forEach((el: HTMLPreElement) => {
+        let defaultCode = (el.firstChild as HTMLElement).innerHTML;
+        while (
+            defaultCode.split('\n').every((line) => line === '' || line.startsWith(' '))
+            && !defaultCode.split('\n').every((line) => line === '')
+            ) {
+            defaultCode = defaultCode.split('\n').map((line) => line.slice(1)).join('\n');
+        }
+        defaultCode = defaultCode.trim() + '\n';
+        const editorContainer = <div class="editor-container"></div>;
+        el.replaceWith(<div class={el.className} id={el.id}>{editorContainer}</div>);
+        editors[el.id] = editor.create(editorContainer, {
+            value: defaultCode,
+            language: el.getAttribute('lang') ?? "python",
+            ...monacoCommonOptions,
+        });
+    });
+
+    const updateFunctions: Record<string, () => Promise<void>> = {};
+    [...c('output')].forEach((el: HTMLDivElement) => {
+        const id = el.getAttribute('for')!;
+        const editor = editors[id];
+        const updateOutput = async () => {
+            try {
+                const code = editor.getValue();
+                const updateSelf = () => updateFunctions[id]();
+                let newContent: HTMLElement[];
+                switch (el.getAttribute('type')) {
+                    case 'code': {
+                        const res = await fetch(`${backendUrl}/cgi-bin/latex.py`, {
+                            method: "POST",
+                            body: code,
+                        });
+                        const text = await res.text();
+                        newContent = [
+                            <button class="mode-toggle" onclick={($event: MouseEvent) => {
+                                ($event.target as HTMLButtonElement).hidden = true;
+                                el.setAttribute('type', 'render');
+                                updateSelf();
+                            }}>Render</button>,
+                            <pre class="latex">{text}</pre>
+                        ];
+                        break;
+                    }
+                    case 'render': {
+                        const res = await fetch(`${backendUrl}/cgi-bin/pandoc.py`, {
+                            method: "POST",
+                            body: code,
+                        });
+                        const blob = await res.blob();
+                        const url = URL.createObjectURL(blob);
+                        newContent = [
+                            <button class="mode-toggle" onclick={($event: MouseEvent) => {
+                                ($event.target as HTMLButtonElement).hidden = true;
+                                el.setAttribute('type', 'latex');
+                                updateSelf();
+                            }}>Code</button>,
+                            <img src={url}/>
+                        ];
+                        break;
+                    }
+                    case 'mdrender': {
+                        const res = await fetch(`${backendUrl}/cgi-bin/pandoc.py?markdown`, {
+                            method: "POST",
+                            body: code,
+                        });
+                        const blob = await res.blob();
+                        const url = URL.createObjectURL(blob);
+                        newContent = [
+                            <button class="mode-toggle" onclick={($event: MouseEvent) => {
+                                ($event.target as HTMLButtonElement).hidden = true;
+                                el.setAttribute('type', 'mdcode');
+                                updateSelf();
+                            }}>Code</button>,
+                            <img src={url}/>
+                        ];
+                        break;
+                    }
+                    case 'mdcode': {
+                        const res = await fetch(`${backendUrl}/cgi-bin/pandoc.py?markdown&code`, {
+                            method: "POST",
+                            body: code,
+                        });
+                        const text = await res.text();
+                        newContent = [
+                            <button class="mode-toggle" onclick={($event: MouseEvent) => {
+                                ($event.target as HTMLButtonElement).hidden = true;
+                                el.setAttribute('type', 'mdrender');
+                                updateSelf();
+                            }}>Render</button>,
+                            <pre class="latex">{text}</pre>
+                        ];
+                        break;
+                    }
+                    default:
+                        throw new Error(`Unknown output type ${el.getAttribute('type')}`);
+                }
+                if (editor.getValue() === code) {
+                    el.replaceChildren(...newContent);
+                }
+            } catch (e) {
+                el.replaceChildren(<div class="error">Error: {e}</div>);
+                throw e;
+            }
+        }
+        updateFunctions[id] = updateOutput;
+        editor.onDidChangeModelContent(updateOutput);
+    });
+
+    // Connection test (and initial rendering)
+
+    const fns = Object.values(updateFunctions);
+    progress.max = fns.length;
+    Promise.all(fns.map(async (f) => {
+        await f();
+        progress.value++;
+    })).then(() => {
+        resultText.innerText = "OK";
+        resultText.style.color = "green";
+        setTimeout(() => {
+            g('connection-test').remove();
+        }, 1000);
+    }).catch(() => {
+        resultText.innerText = "Fehler!";
+        resultText.style.color = "red";
+    });
+});
