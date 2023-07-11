2a928c5a8adab01bad01a0e5569e91913d616c66
nzbr <mail@nzbr.de>
2023-07-11 04:14:57 +0200

    deploy to pages

---
+++ Hack/dist/javascript.40c2f51e.js
@@ -0,0 +1,8 @@
+function e(e,t,a,n){Object.defineProperty(e,t,{get:a,set:n,enumerable:!0,configurable:!0})}var t=("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire4ef6;t.register("k07rR",(function(a,n){e(a.exports,"conf",(()=>o)),e(a.exports,"language",(()=>s));
+/*!-----------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.40.0(83b3cf23ca80c94cccca7c5b3e48351b220f8e35)
+ * Released under the MIT license
+ * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
+ *-----------------------------------------------------------------------------*/
+var i=t("2JSU8"),o=i.conf,s={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","static","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:i.language.operators,symbols:i.language.symbols,escapes:i.language.escapes,digits:i.language.digits,octaldigits:i.language.octaldigits,binarydigits:i.language.binarydigits,hexdigits:i.language.hexdigits,regexpctl:i.language.regexpctl,regexpesc:i.language.regexpesc,tokenizer:i.language.tokenizer}}));
